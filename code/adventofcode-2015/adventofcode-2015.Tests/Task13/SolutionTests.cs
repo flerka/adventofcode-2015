6593c3737d0c8fd5725abd480ce60db05992a8c1
Marya <marya.kozhemyakina@gmail.com>
2022-05-06 11:58:25 +0300

    add day 7 tasks

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task13/SolutionTests.cs
@@ -0,0 +1,69 @@
+﻿using adventofcode_2015.Task13;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task13
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task13_RealExample_Correct()
+        {
+            Assert.Equal(-457, Solution.Function(ReadFile(Path.Combine("Task13", "Data.txt"))));
+        }
+
+        private List<Command> ReadFile(string fileName)
+        {
+            return File.ReadAllLines(fileName).Select<string, Command>(item =>
+            {
+                int n;
+
+                var data = item.Split(" -> ").ToList();
+                string output = data[1];
+
+                if (item.Contains($" {OperationType.AND} "))
+                {
+                    var temp = data[0].Split($" {OperationType.AND} ").ToList();
+                    if (int.TryParse(temp[0], out n))
+                    {
+                        return new AndCommand(n, temp[1], output);
+                    }
+                    return new AndCommand(temp[0], temp[1], output);
+                }
+               
+                if (item.Contains($" {OperationType.OR} "))
+                {
+                    var temp = data[0].Split($" {OperationType.OR} ").ToList();
+                    return new OrCommand(temp[0], temp[1], output);
+                }
+
+                if (item.Contains($" {OperationType.LSHIFT} "))
+                {
+                    var temp = data[0].Split($" {OperationType.LSHIFT} ").ToList();
+                    return new LShiftComamnd(temp[0], int.Parse(temp[1]), output);
+                }
+
+                if (item.Contains($" {OperationType.RSHIFT} "))
+                {
+                    var temp = data[0].Split($" {OperationType.RSHIFT} ").ToList();
+                    return new RShiftComamnd(temp[0],int.Parse(temp[1]), output);
+                }
+
+                if (item.Contains($"{OperationType.NOT}"))
+                {
+                    return new NotCommand(data[0].Split($"{OperationType.NOT} ")[1], output);
+                }
+
+                if(int.TryParse(data[0], out n))
+                {
+                    return new InputCommand(int.Parse(data[0]), output);
+                }
+
+                return new InputCommand(data[0], output);
+            }).ToList();
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015/Task14/Solution.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode_2015.Task14
+{
+    public class Command
+    {
+        public Command(string op, string left, string right, string output)
+        {
+            Op = op;
+            Output = output;
+            Left = left;
+            Right = right;
+        }
+
+        public virtual int PerformCommand(int? left, int? right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Output { get; set; }
+        public string Op { get; set; }
+        public string? Left { get; set; }
+        public string? Right { get; set; }
+    }
+
+    public class RShiftComamnd : Command
+    {
+        public RShiftComamnd(string left, int shift, string output) : base(OperationType.RSHIFT, left, null, output)
+        {
+            ShiftVal = shift;
+        }
+        public int ShiftVal { get; set; }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return left.Value >> ShiftVal;
+        }
+    }
+    public class LShiftComamnd : Command
+    {
+        public LShiftComamnd(string left, int shift, string output) : base(OperationType.RSHIFT, left, null, output)
+        {
+            ShiftVal = shift;
+        }
+        public int ShiftVal { get; set; }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return left.Value << ShiftVal;
+        }
+    }
+
+    public class NotCommand : Command
+    {
+        public NotCommand(string left, string output) : base(OperationType.NOT, left, null, output)
+        { 
+        }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return  ~ left.Value;
+        }
+    }
+
+    public class InputCommand : Command
+    {
+        public InputCommand(int input, string output) : base(OperationType.INPUT, null, null, output)
+        {
+            InputVal = input;
+        }
+
+        public InputCommand(string input, string output) : base(OperationType.INPUT, input, null, output)
+        {
+        }
+
+        public int? InputVal { get; set; }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return left.Value;
+        }
+    }
+
+    public class AndCommand : Command
+    {
+        public AndCommand(int left, string right, string output) : base(OperationType.AND, null, right, output)
+        {
+            InputVal = left;
+        }
+
+        public AndCommand(string left, string right, string output) : base(OperationType.AND, left, right, output)
+        {
+        }
+
+        public int? InputVal { get; set; }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return InputVal.HasValue ? InputVal.Value & right.Value : left.Value & right.Value;
+        }
+    }
+
+    public class OrCommand : Command
+    {
+        public OrCommand(string left, string right, string output) : base(OperationType.OR, left, right, output)
+        {
+        }
+
+        public override int PerformCommand(int? left, int? right)
+        {
+            return left.Value | right.Value;
+        }
+    }
+
+    public class OperationType
+    {
+        public const string AND = "AND";
+        public const string OR = "OR";
+        public const string LSHIFT = "LSHIFT";
+        public const string RSHIFT = "RSHIFT";
+        public const string NOT = "NOT";
+        public const string INPUT = "INPUT";
+    }
+
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the second https://adventofcode.com/2015/day/7/ task
+        /// </summary>
+        public static int Function(List<Command> inputData)
+        {
+            var input = inputData.Where(item => item is InputCommand && ((InputCommand)item).InputVal.HasValue).ToList();
+            var operations = inputData.Where(item => !input.Contains(item)).ToList();
+
+            var valuesDict = new Dictionary<string, int>();
+            foreach (var item in input)
+            {
+                valuesDict.Add(item.Output, ((InputCommand)item).InputVal.Value);
+            }
+
+            var a = GetA(operations, valuesDict);
+            valuesDict["b"] = a;
+            return GetA(operations, valuesDict);
+        }
+
+        private static int GetA(List<Command> inputOp, Dictionary<string, int> inputD)
+        {
+            var operations = new List<Command>(inputOp);
+            var valuesDict = new Dictionary<string, int>(inputD);
+            while (operations.Count > 0)
+            {
+                var op = operations.First(item =>
+                    (string.IsNullOrEmpty(item.Left) || valuesDict.ContainsKey(item.Left))
+                        && (string.IsNullOrEmpty(item.Right) || valuesDict.ContainsKey(item.Right)));
+
+                int? left = string.IsNullOrEmpty(op.Left) ? null : valuesDict[op.Left];
+                int? right = string.IsNullOrEmpty(op.Right) ? null : valuesDict[op.Right];
+                var result = op.PerformCommand(left, right);
+                operations.Remove(op);
+                valuesDict[op.Output] = result;
+            }
+
+            return valuesDict["a"];
+        }
+    }
+}
