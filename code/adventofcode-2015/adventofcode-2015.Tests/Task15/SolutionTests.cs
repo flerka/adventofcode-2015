f6c96931e3e5c085fe75dba3d3844f15a5a54634
Marya <marya.kozhemyakina@gmail.com>
2022-05-08 12:31:07 +0300

    add day 8 solutions

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task15/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using adventofcode_2015.Task15;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task15
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task15_RealExample_Correct()
+        {
+            Assert.Equal(12, Solution.Function(ReadFile(Path.Combine("Task15", "Data.txt"))));
+        }
+
+        private List<string> ReadFile(string fileName)
+        {
+            return File.ReadAllLines(fileName).ToList();
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task16/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using adventofcode_2015.Task16;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task16
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task16_RealExample_Correct()
+        {
+            Assert.Equal(19, Solution.Function(ReadFile(Path.Combine("Task16", "Data.txt"))));
+        }
+
+        private List<string> ReadFile(string fileName)
+        {
+            return File.ReadAllLines(fileName).ToList();
+        }
+    }
+}
