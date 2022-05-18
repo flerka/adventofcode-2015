3c43788529ae00ff2ce8e1d83c25fa9c7f45b5d7
Marya <marya.kozhemyakina@gmail.com>
2022-05-18 19:11:39 +0300

    add day 10 solution

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task19/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using adventofcode_2015.Task19;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task19
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task19_RealExample_Correct()
+        {
+            Assert.Equal("312211", Solution.Function("111221", 1));
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015/Task19/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adventofcode_2015.Task19
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the first and the https://adventofcode.com/2015/day/10/ task
+        /// </summary>
+        public static string Function(string input, int repeat)
+        {
+            var word = input;
+            for (int i = 0; i < repeat; i++)
+            {
+                var newWord = new StringBuilder(10000000);
+                char tempN = word[0];
+                int tempC = 1;
+
+                for (int k = 1; k < word.Length; k++)
+                {
+                    if (word[k] == tempN)
+                    {
+                        tempC++;
+                    }
+                    else 
+                    {
+                        newWord.Append($"{tempC}{tempN}");
+                        tempC = 1;
+                        tempN = word[k];
+                    }
+                }
+                newWord.Append($"{tempC}{tempN}");
+                word = newWord.ToString();
+            }
+
+            return word;
+        }
+    }
+}
