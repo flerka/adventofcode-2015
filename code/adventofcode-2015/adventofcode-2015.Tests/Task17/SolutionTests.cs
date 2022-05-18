7fba35bdb3bfd2fbc123038f735a1fc81976a781
Marya <marya.kozhemyakina@gmail.com>
2022-05-18 17:34:03 +0300

    add day 9 solutions

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task17/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using adventofcode_2015.Task17;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task17
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task17_RealExample_Correct()
+        {
+            Assert.Equal(605, Solution.Function(ReadFile(Path.Combine("Task17", "Data.txt"))));
+        }
+
+        private List<(string, string, double)> ReadFile(string fileName)
+        {
+            return File.ReadAllLines(fileName).Select(item =>
+            {
+                var temp = item.Split(" = ");
+                var distance = double.Parse(temp[1]);
+
+                var cities = temp[0].Split(" to ");
+                return (cities[0], cities[1], distance);
+            }).ToList();
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task18/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using adventofcode_2015.Task18;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task18
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task18_RealExample_Correct()
+        {
+            Assert.Equal(982, Solution.Function(ReadFile(Path.Combine("Task18", "Data.txt"))));
+        }
+
+        private List<(string, string, double)> ReadFile(string fileName)
+        {
+            return File.ReadAllLines(fileName).Select(item =>
+            {
+                var temp = item.Split(" = ");
+                var distance = double.Parse(temp[1]);
+
+                var cities = temp[0].Split(" to ");
+                return (cities[0], cities[1], distance);
+            }).ToList();
+        }
+    }
+}
