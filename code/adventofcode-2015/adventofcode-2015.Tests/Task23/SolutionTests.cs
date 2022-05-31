ecaa2835de1094d93ce36646d8334c5d26c31ee0
Marya <marya.kozhemyakina@gmail.com>
2022-05-31 23:06:18 +0300

    add day 14 solution

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task23/SolutionTests.cs
@@ -0,0 +1,45 @@
+﻿using adventofcode_2015.Task23;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task23
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task23_RealExample_Correct()
+        {
+            Assert.Equal(330, Solution.Function(ReadFile(Path.Combine("Task23", "Data.txt"))));
+        }
+
+        private List<(string, string, int)> ReadFile(string fileName)
+        {
+            var size = 3;
+            var lines = File.ReadAllLines(fileName);
+            var skip = 0;
+
+            var result = new List<(string, string, int)>();
+            while (skip < lines.Length)
+            {
+                var namedLines = lines.Skip(skip).Take(size);
+                foreach(var line in namedLines)
+                {
+                    var words = line.TrimEnd('.').Split(" ");
+                    var name = words[0];
+                    var neighbor = words.Last();
+
+                    var number = int.Parse(words[3]);
+                    number *= words[2] == "gain" ? 1 : -1;
+
+                    result.Add((name, neighbor, number));
+                }
+
+                skip += size;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015/Task24/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace adventofcode_2015.Task24
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the second https://adventofcode.com/2015/day/13/ task
+        /// </summary>
+        public static int Function(List<(string name, string neigh, int sat)> input)
+        {
+            var names = input.GroupBy(i => i.name).Select(i => i.Key).ToList();
+            var pairs = input.ToDictionary(i => (i.name, i.neigh), k => k.sat);
+            var mutations = GetAllNamesPermutations(names, 5);
+
+            return mutations.Select(mut =>
+            {
+                var mutationsList = mut.ToList();
+                var res = 0;
+                for (int i = 1; i < mutationsList.Count - 1; i++)
+                {
+                    res += pairs[(mutationsList[i], mutationsList[i - 1])];
+                    res += pairs[(mutationsList[i], mutationsList[i + 1])];
+                }
+                res += pairs[(mutationsList[0], mutationsList[mutationsList.Count - 1])];
+                res += pairs[(mutationsList[0], mutationsList[1])];
+
+                res += pairs[(mutationsList[mutationsList.Count-1], mutationsList[mutationsList.Count - 2])];
+                res += pairs[(mutationsList[mutationsList.Count - 1], mutationsList[0])];
+                return res;
+            }).Max();
+        }
+
+        // based on this answer https://stackoverflow.com/a/10629938
+        static IEnumerable<IEnumerable<string>> GetAllNamesPermutations(IEnumerable<string> list, int length)
+        {
+            if (length == 1) return list.Select(t => new string[] { t });
+
+            return GetAllNamesPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat(new string[] { t2 }));
+        }
+    }
+}
