fb59fe395994ba8594a303224715992a5b806e67
Mary <marya.kozhemyakina@gmail.com>
2022-06-04 11:41:29 +0300

    add day 14 solution

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task25/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using adventofcode_2015.Task25;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task25
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task25_RealExample_Correct()
+        {
+            var data = ReadFile(Path.Combine("Task25", "Data.txt"));
+            Assert.Equal(1120, Solution.Function(data, 1000));
+        }
+
+        private List<HorseStats> ReadFile(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+
+            var result = new List<HorseStats>();
+
+            foreach (var line in lines)
+            {
+                var words = line.TrimEnd('.').Split(" ");
+                var name = words[0];
+                var speed = int.Parse(words[3]);
+                var runDur = int.Parse(words[6]);
+                var restDur = int.Parse(words[words.Length - 2]);
+
+                result.Add(new HorseStats(name, speed, runDur, restDur));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task26/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using adventofcode_2015.Task26;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task26
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task26_RealExample_Correct()
+        {
+            var data = ReadFile(Path.Combine("Task26", "Data.txt"));
+            Assert.Equal(689, Solution.Function(data, 1000));
+        }
+
+        private List<HorseStats> ReadFile(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+
+            var result = new List<HorseStats>();
+
+            foreach (var line in lines)
+            {
+                var words = line.TrimEnd('.').Split(" ");
+                var name = words[0];
+                var speed = int.Parse(words[3]);
+                var runDur = int.Parse(words[6]);
+                var restDur = int.Parse(words[words.Length - 2]);
+
+                result.Add(new HorseStats(name, speed, runDur, restDur));
+            }
+
+            return result;
+        }
+    }
+}
