4c75a444f53ecd8cf4c3628811855cd5590dbf25
Mary <marya.kozhemyakina@gmail.com>
2022-06-15 19:05:47 +0300

    add day 15 solutions

---
+++ code/adventofcode-2015/adventofcode-2015/Task30/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode_2015.Task30
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the second https://adventofcode.com/2015/day/16/ task
+        /// </summary>
+        public static int Function(Dictionary<string, int> userData,
+                Dictionary<int, Dictionary<string, int>> data)
+        {
+            return data.Keys.FirstOrDefault(key => data[key].Keys.All(subkey =>
+            {
+                return subkey switch 
+                {
+                    "cats" or "trees" => data[key][subkey] > userData[subkey],
+                    "pomeranians" or "goldfish" => data[key][subkey] < userData[subkey],
+                    _ => data[key][subkey] == userData[subkey]
+                };
+            }));
+        }
+    }
+}
