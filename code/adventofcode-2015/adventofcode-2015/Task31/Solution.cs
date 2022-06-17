e79e7683ef5078418f9816e4325e4fb2e44d98d6
Mary <marya.kozhemyakina@gmail.com>
2022-06-17 17:59:08 +0300

    add day 17 solution

---
+++ code/adventofcode-2015/adventofcode-2015/Task31/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode_2015.Task31
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the first https://adventofcode.com/2015/day/17/ task
+        /// </summary>
+        public static int Function(List<int> input, int size)
+        {
+            var result = 0;
+
+            var count = Math.Pow(2, input.Count);
+            for (var i = 1; i <= count - 1; i++)
+            {
+                var temp = 0;
+                var str = Convert.ToString(i, 2).PadLeft(input.Count, '0');
+                for (var j = 0; j < str.Length; j++)
+                {
+                    if (temp > size)
+                    {
+                        break;
+                    }
+
+                    if (str[j] == '1')
+                    {
+                        temp += input[j];
+                    }
+                }
+
+                if (temp == size)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
