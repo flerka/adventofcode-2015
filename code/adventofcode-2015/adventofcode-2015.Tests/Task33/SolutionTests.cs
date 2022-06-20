94628d5b84b565994019e75d2839f0d305baeeb5
Mary <marya.kozhemyakina@gmail.com>
2022-06-20 19:37:28 +0300

    add day 18 solutions

---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task33/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using adventofcode_2015.Task33;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task33
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task33_RealExample_Correct()
+        {
+            var data = ReadFile(Path.Combine("Task33", "Data.txt"));
+            var stepsCount = 100;
+            Assert.Equal(4, Solution.Function(data, stepsCount));
+        }
+
+        private List<List<bool>> ReadFile(string fileName)
+        {
+            var result = new List<List<bool>>();
+            var lines = File.ReadAllLines(fileName);
+            foreach (var line in lines)
+            {
+                result.Add(line.ToCharArray().Select(i => i == '#').ToList());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ code/adventofcode-2015/adventofcode-2015.Tests/Task34/SolutionTests.cs
@@ -0,0 +1,32 @@
+﻿using adventofcode_2015.Task34;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task34
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task34_RealExample_Correct()
+        {
+            // To make my solution work I manually changed corner values in input data to true.
+            var data = ReadFile(Path.Combine("Task34", "Data.txt"));
+            var stepsCount = 100;
+            Assert.Equal(7, Solution.Function(data, stepsCount));
+        }
+
+        private List<List<bool>> ReadFile(string fileName)
+        {
+            var result = new List<List<bool>>();
+            var lines = File.ReadAllLines(fileName);
+            foreach (var line in lines)
+            {
+                result.Add(line.ToCharArray().Select(i => i == '#').ToList());
+            }
+
+            return result;
+        }
+    }
+}
