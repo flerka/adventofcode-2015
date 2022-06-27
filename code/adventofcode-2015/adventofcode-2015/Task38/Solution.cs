5756f7327b95fac66f74724829ec07e619545962
Mary <marya.kozhemyakina@gmail.com>
2022-06-27 22:13:46 +0300

    add day 20 long solution

---
+++ code/adventofcode-2015/adventofcode-2015/Task38/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace adventofcode_2015.Task38
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the second https://adventofcode.com/2015/day/20/ task
+        /// </summary>
+        public static int Function(long presentsNumber)
+        {
+            var n = presentsNumber / 10;
+            Dictionary<int, int> elfes = new();
+            for(var j = 1; j < n; j++)
+            {
+                elfes[j] = 0;
+                var sum = 0;
+
+                for (var i = 1; (i <= j); i++)
+                {
+                    if (elfes[i] < 51 && j % i == 0)
+                    {
+                        elfes[i]++;
+                        sum += i * 11;
+                    }
+                }
+
+                if (sum >= presentsNumber)
+                {
+                    return j;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
