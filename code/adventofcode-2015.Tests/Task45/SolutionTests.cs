bfd9c2dda06a59947e0b2cf364baddc2fa2b268b
Mary <marya.kozhemyakina@gmail.com>
2022-07-27 10:36:16 +0300

    add day 24 solution

---
+++ code/adventofcode-2015.Tests/Task45/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using adventofcode_2015.Task45;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task45
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task45_RealExample_Correct()
+        {
+            Assert.Equal(25360703L, Solution.Function(ReadFileAsync(Path.Combine("Task45", "Data.txt"))));
+        }
+
+        private List<long> ReadFileAsync(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            return lines.Select(line =>
+            {
+                return long.Parse(line);
+            }).ToList();
+        }
+    }
+}
---
+++ code/adventofcode-2015.Tests/Task46/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using adventofcode_2015.Task46;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace adventofcode_2015.Tests.Task46
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Task46_RealExample_Correct()
+        {
+            Assert.Equal(24104369L, Solution.Function(ReadFileAsync(Path.Combine("Task46", "Data.txt"))));
+        }
+
+        private List<long> ReadFileAsync(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            return lines.Select(line =>
+            {
+                return long.Parse(line);
+            }).ToList();
+        }
+    }
+}
